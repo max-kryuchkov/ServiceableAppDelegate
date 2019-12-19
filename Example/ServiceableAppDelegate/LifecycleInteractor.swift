21f82084a85fac83983073f199f3aa457da952ad
max kruchkov <max@maxs-MacBook-Pro.local>
2019-12-19 12:25:22 +0200

    init

---
+++ Example/ServiceableAppDelegate/LifecycleInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  LifecycleInteractor.swift
+//  ServiceableAppDelegate_Example
+//
+//  Created by max on 19.12.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
